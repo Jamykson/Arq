a09d9a56475194101df6537253fd7cfa9ece2bcd
Jamykson Freitas <105256874+Jamykson@users.noreply.github.com>
2026-05-29 02:40:00 +0000

    Updates

--- server/assets/_tanstack-start-manifest_v-DkFySBYz.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/workspaces/Arq/cecilia-studio-launchpad/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BPV1Adnv.js"] }, "/": { filePath: "/workspaces/Arq/cecilia-studio-launchpad/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BG9F6flV.js"] } }, clientEntry: "/assets/index-BPV1Adnv.js" });
-export {
-  tsrStartManifest
-};
